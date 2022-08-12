861b6c71c885f54707ac3e012b59da3986bd1db9
git config --global user.name Your <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>
2022-08-12 11:30:59 -0700

    fix :)

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,9 @@
 $str = 'I love Puppet'
 
 file { '/tmp/school':
-owner   => 'www-data',
-group   => 'www-data',
-mode    => '0744',
-content => $str,
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => $str,
 }
