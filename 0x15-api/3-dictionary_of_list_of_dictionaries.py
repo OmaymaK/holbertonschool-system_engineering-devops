b86ac2aa4c547f99fd6ef9cef091a1818f69fad1
OmaymaK <emmablink050@gmail.com>
2022-09-12 22:11:15 +0100

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Export data in the JSON format."""
+import json
+import requests
+
+
+if __name__ == "__main__":
+
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    udict = {}
+
+    for user in users:
+        todos = requests.get("""
+                https://jsonplaceholder.typicode.com/users/{}/todos
+                """.format(str(user.get('id')))).json()
+        udict[str(user.get('id'))] = []
+        for todo in todos:
+            td = {"username": user.get('username'),
+                  "task": todo.get('title'),
+                  "completed": todo.get('completed')}
+            udict[str(user.get('id'))].append(td)
+
+    with open("todo_all_employees.json", 'w') as file:
+        json.dump(udict, file)
