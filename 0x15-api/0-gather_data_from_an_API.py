e3e9005a2995b76b46b3c85fff9216ec6524575f
OmaymaK <emmablink050@gmail.com>
2022-09-12 21:44:52 +0100

    pycodestyle fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Returns information about TODO list progress."""
-import sys
 import requests
-
+from sys import argv
 
 if __name__ == "__main__":
     todos = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
-                    .format(sys.argv[1]))
+                         .format(argv[1]))
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}/"
-                    .format(sys.argv[1]))
+                        .format(argv[1]))
     todos = todos.json()
     user = user.json()
     done = []
@@ -17,6 +16,6 @@ if __name__ == "__main__":
         if todo.get('completed'):
             done.append(todo)
     print("Employee {} is done with tasks({}/{}):"
-            .format(user.get('name'), len(done), len(todos)))
+          .format(user.get('name'), len(done), len(todos)))
     for todo in done:
         print("\t {}".format(todo.get('title')))
