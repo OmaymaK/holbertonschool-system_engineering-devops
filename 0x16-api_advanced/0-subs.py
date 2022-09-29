ac05f41c6ad8848628980ad03d5c3b4adb4ae989
OmaymaK <emmablink050@gmail.com>
2022-09-29 20:22:38 +0100

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Returns the number of subscribers."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Get number of subs."""
+    headers = {'User-Agent': "Mozilla/5.0"}
+    r = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
+                       headers=headers, allow_redirects=False)
+    if r.status_code == 302:
+        return 0
+    d = r.json()
+    try:
+        s = d['data']['subscribers']
+    except Exception:
+        return 0
+    return s
