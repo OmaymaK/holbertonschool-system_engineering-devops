8370877749d51d3c382b366aaee6daca9fd9d331
OmaymaK <emmablink050@gmail.com>
2022-09-29 20:55:06 +0100

    task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,23 @@
+"""Returns a list with the titles of hot articles for a given subreddit."""
+
+
+import requests
+
+
+def recurse(subreddit, after='', hot_list=[]):
+    """Returns a list."""
+    headers = {'User-Agent': "Mozilla/5.0"}
+    url = 'https://www.reddit.com'
+    r = requests.get(f'{url}/r/{subreddit}/hot.json?after={after}',
+                     headers=headers, allow_redirects=False)
+    if r.status_code == 302:
+        return None
+    d = r.json()
+    if d.get('data') is None:
+        return None
+    for post in d['data']['children']:
+        title = post['data']['title']
+        hot_list.append(title)
+    if d['data']['after'] is None:
+        return hot_list
+    return recurse(subreddit, d['data']['after'], hot_list)
