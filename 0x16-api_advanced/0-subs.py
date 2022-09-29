34710178e68e8258fc211f44249e40ff9835d03e
OmaymaK <emmablink050@gmail.com>
2022-09-29 20:24:45 +0100

    pycodestyle fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Get number of subs."""
     headers = {'User-Agent': "Mozilla/5.0"}
     r = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
-                       headers=headers, allow_redirects=False)
+                     headers=headers, allow_redirects=False)
     if r.status_code == 302:
         return 0
     d = r.json()
